1a2de4950a7287ee44e33dce079049ad3e97692e
Shivangi <shivangi.das7@gmail.com>
2020-06-08 18:15:04 +0100

    download model script

---
+++ download_uncased_base.sh
@@ -0,0 +1,5 @@
+#! bin/bash
+
+curl https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip > uncased_L-12_H-768_A-12.zip        && \
+unzip uncased_L-12_H-768_A-12.zip  && \
+rm uncased_L-12_H-768_A-12.zip
