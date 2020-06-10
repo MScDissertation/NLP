c09a4ab93618e985e571b2a19e7ed5791b698307
Shivangi <shivangi.das7@gmail.com>
2020-06-10 16:16:29 +0100

    running together

--- fine_tune_example.sh
+++ fine_tune_example.sh
@@ -1,26 +1,35 @@
 #! bin/bash
 
 export GLUE_DIR=data
+export TASK=MRPC
 export BATCH_SIZE=32
-if [ "$#" -eq  "0" ]
+export MAX_SEQ_LEN=128
+export MODEL=bert-base-cased
+export LEARNING_RATE=2e-5
+if [ "$1" ]
 then
-  export TASK=MRPC
-else
   export TASK=$1
 fi
-
 if [ "$2" ]
 then
   export BATCH_SIZE=$2
 fi
+if [ "$3" ]
+then
+  export MAX_SEQ_LEN=$3
+fi
+if [ "$4" ]
+then
+  export MODEL=$4
+fi
 python ./transformers/examples/text-classification/run_glue.py \
-  --model_name_or_path bert-base-cased \
+  --model_name_or_path $MODEL \
   --task_name $TASK \
   --do_train \
   --do_eval \
   --data_dir $GLUE_DIR/$TASK/ \
-  --max_seq_length 128 \
+  --max_seq_length $MAX_SEQ_LEN \
   --per_device_train_batch_size $BATCH_SIZE \
-  --learning_rate 2e-5 \
+  --learning_rate $LEARNING_RATE \
   --num_train_epochs 3.0 \
   --output_dir /tmp/${TASK}_output/ --overwrite_output_dir
---
+++ train_and_record_power.sh
@@ -0,0 +1,26 @@
+#! bin/bash
+
+#task_batch_seq_model
+export TASK=MRPC
+export BATCH_SIZE=32
+export MAX_SEQ_LEN=128
+export MODEL=bert-base-cased
+if [ "$1" ]
+then
+  export TASK=$1
+fi
+if [ "$2" ]
+then
+  export BATCH_SIZE=$2
+fi
+if [ "$3" ]
+then
+  export MAX_SEQ_LEN=$3
+fi
+if [ "$4" ]
+then
+  export MODEL=$4
+fi
+sh nvidiasmi.sh ${TASK}_${BATCH_SIZE}_${MAX_SEQ_LEN}
+sh fine_tune_example.sh $TASK $BATCH_SIZE $MAX_SEQ_LEN $MODEL
+pkill nvidia-smi
