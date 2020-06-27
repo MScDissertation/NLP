06dc4b5c7c1033b5d3d8e8e472a9e21009b4b1c5
Shivangi <shivangi.das7@gmail.com>
2020-06-27 21:22:03 +0100

    download more data

--- downloadwikidump.sh
+++ downloadwikidump.sh
@@ -1,3 +1,3 @@
 #! bin/bash
 
-curl https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2 --output wikidownload.xml.bz2
+curl https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2 --output /media/data/wikidownload.xml.bz2
