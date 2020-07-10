3dfb20486492df8a452e4916abf71dd6c40ac6e4
Shivangi <shivangi.das7@gmail.com>
2020-07-10 20:55:38 +0100

    send for testing

---
+++ fine_tune_gpt2.sh
@@ -0,0 +1,12 @@
+export TRAIN_FILE=wikitext-2-raw/wiki.train.raw
+export TEST_FILE=wikitext-2-raw/wiki.test.raw
+
+python ./transformers/examples/language-modeling/run_language_modeling.py \
+    --output_dir=GPT/weights \
+    --model_type=gpt2 \
+    --model_name_or_path=gpt2 \
+    --do_train \
+    --train_data_file=$TRAIN_FILE \
+    --do_eval \
+    --eval_data_file=$TEST_FILE \
+    --per_device_train_batch_size=16 \
