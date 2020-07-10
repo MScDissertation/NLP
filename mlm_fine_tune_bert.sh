b0d920dc4eed9b1636bc09b468c00429e2b57fc5
Shivangi <shivangi.das7@gmail.com>
2020-07-10 17:08:04 +0100

    trials with roberta

--- mlm_fine_tune_bert.sh
+++ mlm_fine_tune_bert.sh
@@ -3,11 +3,12 @@ export TEST_FILE=wikitext-2-raw/wiki.test.raw
 
 python ./transformers/examples/language-modeling/run_language_modeling.py \
     --output_dir=output \
-    --model_type=bert \
-    --model_name_or_path=bert-base-uncased \
+    --overwrite_output_dir \
+    --model_type=roberta \
+    --model_name_or_path=roberta-base \
     --do_train \
     --train_data_file=$TRAIN_FILE \
     --do_eval \
     --eval_data_file=$TEST_FILE \
-    --per_device_train_batch_size=1 \
+    --per_device_train_batch_size=2 \
     --mlm
