11c69e31cb73d5e2866d1a70fac68da0ae75af8e
Shivangi <shivangi.das7@gmail.com>
2020-07-15 16:03:55 +0100

    sqaud version 2

--- download_squad.sh
+++ download_squad.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 export DIR=SQUAD
 mkdir $DIR
-curl https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json --output $DIR/train-v1.1.json
-curl https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json --output $DIR/dev-v1.1.json
-curl https://github.com/allenai/bi-att-flow/blob/master/squad/evaluate-v1.1.py --output $DIR/dev-v1.1.json
+curl https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json --output $DIR/train-v2.0.json
+curl https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json --output $DIR/dev-v2.0.json
+curl https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/ --output $DIR/evaluate-v2.0.json
