ed217fd081ed96ef7fd736207186b4370fb7881d
Shivangi <shivangi.das7@gmail.com>
2020-07-16 18:08:20 +0100

    overwrite

--- modelsFT/gpt2-medium.sh
+++ modelsFT/gpt2-medium.sh
@@ -21,4 +21,5 @@ python ../transformers/examples/language-modeling/run_language_modeling.py \
     --train_data_file=$TRAIN_FILE \
     --do_eval \
     --eval_data_file=$TEST_FILE \
-    --per_device_train_batch_size=$BATCH_SIZE
+    --per_device_train_batch_size=$BATCH_SIZE \
+    --overwrite_output_dir
