7a8e90e27ed348a16f04685ab341917b1183d8ab
Shivangi <shivangi.das7@gmail.com>
2020-07-23 17:51:50 +0100

    test with uniformly varying time

---
+++ train_for_different_time.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+# 2 hours
+export TASK=QQP
+export time_duration="10 15 20 25 30 35 40 45 50 55 60"
+for time in ${time_duration}; do
+    sh nvidiasmi.sh ${TASK}_64_128 &
+    timeout ${time} sh fine_tune_example.sh $TASK 64 128
+    echo "Exiting"
+    pkill nvidia-smi
+    echo "New power consumption recorded"
+done
