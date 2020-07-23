cabc7468bbc361b59c4cb6b598492e7021e30286
Shivangi <shivangi.das7@gmail.com>
2020-07-23 17:53:36 +0100

    store time in name

--- train_for_different_time.sh
+++ train_for_different_time.sh
@@ -3,7 +3,7 @@
 export TASK=QQP
 export time_duration="10 15 20 25 30 35 40 45 50 55 60"
 for time in ${time_duration}; do
-    sh nvidiasmi.sh ${TASK}_64_128 &
+    sh nvidiasmi.sh ${TASK}_64_${time} &
     timeout ${time} sh fine_tune_example.sh $TASK 64 128
     echo "Exiting"
     pkill nvidia-smi
